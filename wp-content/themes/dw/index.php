7b13dfdeaee4b141076b2afb29b6f4eb00b3d16d
MEEX-Valentin <valentin.meex@student.hepl.Be>
2025-02-14 09:32:11 +0100

    Added first iteration example of "the loop"

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -9,7 +9,26 @@
     <title><?= wp_title('·', 'false', 'right') . get_bloginfo('name') ?></title>
 </head>
 <body>
-    <h1><?= get_bloginfo('name'); ?></h1>
-    <p><?= get_bloginfo('description'); ?></p>
+    <header>
+        <h1><?= get_bloginfo('name'); ?></h1>
+        <p><?= get_bloginfo('description'); ?></p>
+    </header>
+    <main>
+        <?php
+        // On ouvre la boucle (the loop), la structure de contrôle
+        // de contenu propre à wordpress:
+        if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <h2><?= get_the_title(); ?></h2>
+
+            <div><?= get_the_content(); ?></div>
+
+        <?php
+            // on ferme "la boucle" (the loop):
+        endwhile;
+        else: ?>
+            <p>La page est vide.</p>
+        <?php endif; ?>
+    </main>
 </body>
 </html>
