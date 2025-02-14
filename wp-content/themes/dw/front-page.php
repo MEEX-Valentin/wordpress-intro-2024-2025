c7590574bba2208937196c55f322d003fd6539b8
MEEX-Valentin <valentin.meex@student.hepl.Be>
2025-02-14 09:47:08 +0100

    Added front-page test

---
+++ wp-content/themes/dw/front-page.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="description">
+    <meta name="keywords" content="keywords">
+    <meta name="Auhtor" content="auhtor">
+    <title><?= wp_title('·', 'false', 'right') . get_bloginfo('name') ?></title>
+</head>
+<body>
+    <header>
+        <h1><?= get_bloginfo('name'); ?></h1>
+        <p><?= get_bloginfo('description'); ?></p>
+    </header>
+    <main>
+            <aside>
+                <h2>Bienvenue sur mon site</h2>
+            </aside>
+        <?php
+        // On ouvre la boucle (the loop), la structure de contrôle
+        // de contenu propre à wordpress:
+        if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <h2><?= get_the_title(); ?></h2>
+
+            <div><?= get_the_content(); ?></div>
+
+        <?php
+            // on ferme "la boucle" (the loop):
+        endwhile;
+        else: ?>
+            <p>La page est vide.</p>
+        <?php endif; ?>
+    </main>
+</body>
+</html>
