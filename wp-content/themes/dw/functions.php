332708f7d22ecc5043c112064e7c9978cad9814e
MEEX-Valentin <valentin.meex@student.hepl.Be>
2025-03-14 08:53:27 +0100

    added nav menu + other things before

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -1,30 +1,30 @@
 <?php
+// Charger les champs ACF exportés :
+include_once('fields.php');
 
-// Gutenberg est le nouvel éditeur de contenu prorpre à wordpress
+// Gutenberg est le nouvel éditeur de contenu propre à Wordpress
 // il ne nous intéresse pas pour l'utilisation du thème que nous
-// allons créer. On va donc le désactiver:
+// allons créer. On va donc le désactiver :
 
 // Disable Gutenberg on the back end.
-add_filter( 'use_block_editor_for_post', '__return_false' );
+add_filter('use_block_editor_for_post', '__return_false');
 // Disable Gutenberg for widgets.
-add_filter( 'use_widgets_block_editor', '__return_false' );
+add_filter('use_widgets_block_editor', '__return_false');
 // Disable default front-end styles.
-add_action( 'wp_enqueue_scripts', function() {
+add_action('wp_enqueue_scripts', function () {
     // Remove CSS on the front end.
-    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style('wp-block-library');
     // Remove Gutenberg theme.
-    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style('wp-block-library-theme');
     // Remove inline global CSS on the front end.
-    wp_dequeue_style( 'global-styles' );
-}, 20 );
+    wp_dequeue_style('global-styles');
+}, 20);
 
 // Activer l'utilisation des vignettes (image de couverture) sur nos post types:
-add_theme_support( 'post-thumbnails', ['recipe']);
-add_theme_support( 'post-thumbnails', ['travel']);
+add_theme_support('post-thumbnails', ['recipe', 'travel']);
 
 // Enregistrer de nouveaux "types de contenu", qui seront stockés dans la table
 // "wp_posts", avec un identifiant de type spécifique dans la colonne "post_type":
-
 
 register_post_type('recipe', [
     'label' => 'Recettes',
@@ -32,7 +32,9 @@ register_post_type('recipe', [
     'menu_position' => 7,
     'menu_icon' => 'dashicons-carrot',
     'public' => true,
-    'rewrite' => ['slug' => 'recettes',],
+    'rewrite' => [
+        'slug' => 'recettes',
+    ],
     'supports' => ['title', 'excerpt', 'editor', 'thumbnail'],
 ]);
 
@@ -42,15 +44,22 @@ register_post_type('travel', [
     'menu_position' => 6,
     'menu_icon' => 'dashicons-airplane',
     'public' => true,
-    'rewrite' => ['slug' => 'voyages',],
+    'rewrite' => [
+        'slug' => 'voyages',
+    ],
     'supports' => ['title', 'excerpt', 'editor', 'thumbnail'],
 ]);
 
+// Paramétrer des tailles d'images pour le générateur de thumbnails de Wordpress :
 
-// Paramètres des tailles d'images pour le générateur de thumbnails de wordpress
-
-// sans recadrage:
+// Sans recadrage :
 add_image_size('travel-side', 420, 420);
-
-// avec recadrage:
+// Avec recadrage :
 add_image_size('travel-header', 1920, 400, true);
+
+
+// Enregistrer les menus de navigation en foncuton de l'endroit où ils sont exploités :
+
+
+register_nav_menu( 'header', 'Le menu de navigation principal en haut de la page.' );
+register_nav_menu( 'footer', 'Le menu de navigation principal de fin de page.' );
